277d5ea6c902b5894cf53bf79b4f4a2060937bbd
afaf <afaf@email.com>
2024-12-24 18:15:49 +0700

    fix: max channel

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -9,13 +9,21 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// QueueOptions defines RabbitMQ queue configurations
+type QueueOptions struct {
+	Durable    bool
+	AutoDelete bool
+	Exclusive  bool
+	NoWait     bool
+	Args       amqp.Table
+}
+
 const (
 	defaultRetryDuration = time.Second * 5
 	defaultURI           = "amqp://guest:guest@localhost:5672/" // Default RabbitMQ URI
 	defaultMaxChannels   = 10                                   // Default maximum number of channels
 )
 
-// TODO: logger interface config
 // Config holds the configuration for connecting to RabbitMQ
 type Config struct {
 	URI           string        // URI for RabbitMQ connection (e.g., "amqp://guest:guest@localhost:5672/")
@@ -27,19 +35,10 @@ type Config struct {
 // Connection wraps the actual AMQP connection and provides reconnection logic
 type Connection struct {
 	*amqp.Connection
-	config       *Config           // Custom configuration for the connection
-	mu           sync.RWMutex      // Mutex to synchronize access to the connection
-	reconnecting bool              // Flag to indicate ongoing reconnection
-	channels     [](*amqp.Channel) // Slice to hold active channels
-}
-
-// QueueOptions defines RabbitMQ queue configurations
-type QueueOptions struct {
-	Durable    bool
-	AutoDelete bool
-	Exclusive  bool
-	NoWait     bool
-	Args       amqp.Table
+	config       *Config         // Custom configuration for the connection
+	mu           sync.RWMutex    // Mutex to synchronize access to the connection
+	reconnecting bool            // Flag to indicate ongoing reconnection
+	channels     []*amqp.Channel // Slice to hold active channels
 }
 
 // NewConnection creates a new Connection object and initializes it with the provided configuration
@@ -94,31 +93,6 @@ func (c *Connection) Connect() error {
 	}
 }
 
-// Reconnect attempts to reconnect if the connection was lost
-func (c *Connection) Reconnect() error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if c.reconnecting {
-		log.Println("Already in the process of reconnecting. Skipping.")
-		return nil
-	}
-
-	c.reconnecting = true
-
-	conn, err := amqp.DialConfig(c.config.URI, *c.config.AMQPConfig)
-	if err == nil {
-		log.Println("Connected to RabbitMQ")
-		c.Connection = conn
-		c.reconnecting = false
-		return nil
-	}
-
-	log.Printf("Failed to reconnect to RabbitMQ: %v. Retrying in 5 seconds.", err)
-	c.reconnecting = false
-	return err
-}
-
 // IsClosed checks if the RabbitMQ connection is closed
 func (c *Connection) IsClosed() bool {
 	c.mu.Lock()
@@ -145,61 +119,68 @@ func (c *Connection) Close() error {
 	return nil
 }
 
+// Reconnect attempts to reconnect if the connection was lost
+func (c *Connection) Reconnect() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.reconnecting {
+		log.Println("Already in the process of reconnecting. Skipping.")
+		return nil
+	}
+
+	c.reconnecting = true
+
+	conn, err := amqp.DialConfig(c.config.URI, *c.config.AMQPConfig)
+	if err == nil {
+		log.Println("Connected to RabbitMQ")
+		c.Connection = conn
+		c.reconnecting = false
+		return nil
+	}
+
+	log.Printf("Failed to reconnect to RabbitMQ: %v. Retrying in 5 seconds.", err)
+	c.reconnecting = false
+	return err
+}
+
 // OpenChannel opens a new channel with retry logic, ensuring it doesn't exceed MaxChannels
 func (c *Connection) OpenChannel() (*amqp.Channel, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
+	// Remove any closed channels from the active list
+	c.cleanupClosedChannels()
 
 	if len(c.channels) >= c.config.MaxChannels {
 		return nil, fmt.Errorf("maximum number of channels (%d) reached", c.config.MaxChannels)
 	}
 
 	var channel *amqp.Channel
-	var err error
-	retryDuration := c.config.RetryDuration
-
-	for attempts := 0; attempts < 5; attempts++ { // Retry up to 5 times, for example
-		channel, err = c.Connection.Channel()
-		if err == nil {
-			// Add the new channel to the list of channels
-			c.channels = append(c.channels, channel)
-			return channel, nil
-		}
-
-		log.Printf("Failed to open channel: %v. Retrying in %v...", err, retryDuration)
-		time.Sleep(retryDuration)
+	channel, err := c.Connection.Channel()
+	if err == nil {
+		// Add the new channel to the list of channels
+		c.channels = append(c.channels, channel)
+		return channel, nil
 	}
+
+	log.Printf("Failed to open channel: %v. ", err)
 
 	return nil, fmt.Errorf("failed to open channel after retries: %w", err)
 }
 
-// ReconnectChannel attempts to reconnect a channel if it's lost
-func (c *Connection) ReconnectChannel(channel *amqp.Channel) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	// Logic for determining if the channel is closed or broken (could use specific error types)
-	if channel != nil && channel.IsClosed() {
-		log.Printf("Channel is closed, attempting to reopen it...")
-		// Try to open a new channel
-		newChannel, err := c.OpenChannel()
-		if err != nil {
-			return fmt.Errorf("failed to reconnect channel: %w", err)
+// cleanupClosedChannels removes closed channels from the list
+func (c *Connection) cleanupClosedChannels() {
+	var openChannels []*amqp.Channel
+	for _, ch := range c.channels {
+		if ch != nil && !ch.IsClosed() {
+			openChannels = append(openChannels, ch)
+		} else if ch != nil {
+			log.Printf("Channel closed, removing from active channels.")
+			_ = ch.Close() // Close the channel if it was marked closed
 		}
-
-		// Replace the closed channel with the new one
-		for i, ch := range c.channels {
-			if ch == channel {
-				c.channels[i] = newChannel
-				break
-			}
-		}
-
-		log.Println("Successfully reconnected channel.")
-		return nil
 	}
-
-	return nil
+	c.channels = openChannels
 }
 
 // failOnError logs an error and exits the application if the error is not nil
